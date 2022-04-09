7c8f6d87ea49e05d5816f68e99bf4f067b988cad
linefmv <alinemaciel018@gmail.com>
2022-04-09 17:12:06 -0300

    feat: create component Resume

---
+++ src/Components/Resume/Resume.jsx
@@ -0,0 +1,9 @@
+import { Name } from './styles';
+
+const Resume = () => (
+    <Name>Aline Viana</Name>
+
+)
+
+
+export default Resume;
---
+++ src/Components/Resume/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+
+export const Name = styled.h3`
+font-size: 14px;
+`;
