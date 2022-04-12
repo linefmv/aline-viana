cb58cf4fabaf9461637ac3d37d12309244e535db
linefmv <alinemaciel018@gmail.com>
2022-04-11 22:24:33 -0300

    feat: config api

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export default async function getData() {
+
+    const response = await axios.get('https://my-json-server.typicode.com/linefmv/about-me-api/db')
+        .then(response => response.data)
+        .catch(error => error);
+    return await response
+}
