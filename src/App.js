91b71fd07222653b498b239e16aa1ca67e79ae29
linefmv <alinemaciel018@gmail.com>
2022-04-11 22:25:07 -0300

    feat: add data config in app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import './App.css'
-
+import './App.css';
+import React, {useEffect, useState} from 'react';
+import getData from './services/api';
 import MenuLeft from "./Components/MenuLeft/MenuLeft";
 import Resume from "./Components/Resume/Resume";
 import MainDescribe from "./Components/MainDescribe/MainDescribe";
 
 
 function App() {
+    let [data, setData] = useState('')
+
+    useEffect(() => {
+        getData()
+            .then(response => setData(response));
+    }, [])
+
+
   return (
     <>
         <main>
         <MenuLeft />
         <MainDescribe />
-        <Resume />
+        <Resume database={data}/>
         </main>
     </>
   );
