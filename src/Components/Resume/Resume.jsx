d840bdd2ce183fcc54958407081fdbceef31ad14
linefmv <alinemaciel018@gmail.com>
2022-04-16 22:10:59 -0300

    feat: add data to Resume and fit style

--- src/Components/Resume/Resume.jsx
+++ src/Components/Resume/Resume.jsx
@@ -1,9 +1,22 @@
-import { Name } from './styles';
+import {Name, History, Content, Image, SocialMedia, Email, Linkedin, Github, SocialImages} from './styles';
 
-const Resume = (props) => {
+import GithubIcon from './../../assets/github.svg';
+import Me from '../../assets/me.jpeg';
+
+const Resume = ({aboutMeData}) => {
     return (
-            <Name>{props.database.aboutMe[0].name}</Name>
-)
+
+        <Content>
+            <Image src={Me}></Image>
+            <Name>{aboutMeData.fullName}</Name>
+            <History>{aboutMeData.history}</History>
+            <SocialMedia>
+                <Email href="alinemaciel018@gmail.com">Email</Email>
+                <Linkedin href="linkedin.com/linefmv">Linkedin</Linkedin>
+                 <Github href="github.com/linefmv">Github</Github>
+            </SocialMedia>
+        </Content>
+    )
 }
 
 export default Resume;
--- src/Components/Resume/styles.js
+++ src/Components/Resume/styles.js
@@ -1,6 +1,38 @@
 import styled from 'styled-components';
 
-
-export const Name = styled.p`
-  font-size: 14px;
+export const Content = styled.aside`
+  width: 250px;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 `;
+
+export const Image = styled.img`
+  width: 150px;
+  align-self: center;
+  border-radius: 2vh;
+`
+export const Name = styled.p`
+  text-align: center;
+  padding: 2vh;
+`;
+
+export const History = styled.p``;
+
+export const SocialMedia = styled.div`
+  width: 100%;
+  max-width: 240px;
+  display: flex;
+  justify-content: space-between;
+  padding: 2vh 0;
+  
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+export const Email = styled.a``;
+export const Linkedin = styled.a``;
+export const Github = styled.a``;
