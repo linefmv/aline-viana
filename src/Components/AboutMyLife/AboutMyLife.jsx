96e7529548346f2e4f967b1802e6cb44de92475c
linefmv <alinemaciel018@gmail.com>
2022-04-24 17:41:52 -0300

    feat: create component about my life

---
+++ src/Components/AboutMyLife/AboutMyLife.jsx
@@ -0,0 +1,24 @@
+import {
+  Component,
+  ComponentTitle,
+  ComponentParagraph,
+} from "./styles";
+
+const AboutMyLife = ({ describeData }) => {
+  return (
+    <>
+      {describeData.map((item) => {
+        return (
+          <>
+          <Component key={item.id}>
+            <ComponentTitle> {item.title}</ComponentTitle>
+            <ComponentParagraph>{item.about}</ComponentParagraph>
+            </Component>
+          </>
+        );
+      })}
+    </>
+  );
+};
+
+export default AboutMyLife;
