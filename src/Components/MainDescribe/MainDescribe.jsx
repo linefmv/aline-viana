4856a04bc210909670caf4f254e5e13d082d7548
linefmv <alinemaciel018@gmail.com>
2022-04-24 20:43:04 -0300

    refactor: remove unused code

--- src/Components/MainDescribe/MainDescribe.jsx
+++ src/Components/MainDescribe/MainDescribe.jsx
@@ -4,45 +4,15 @@ import Studies from "../Studies/Studies";
 
 import {
   MainContent,
-  MainTitle,
-  MainComponent,
-  Component,
-  ComponentTitle,
-  ComponentParagraph,
-  ComponentSecundaryTitle,
-  ComponentLink,
 } from "./styles";
 
 const MainDescribe = ({ describeData }) => {
   console.log(describeData)
   return (
     <MainContent>
-      <MainTitle>General</MainTitle>
       <AboutMyLife describeData={describeData.aboutMyLife}/>
       <Experiences describeData={describeData.experiences}/>
       <Studies describeData={describeData.studies}/>
-      {/* {describeData.map((item) => {
-        return (
-          <MainComponent>
-            <Component key={item.id}>
-              <ComponentSecundaryTitle> {item.title}</ComponentSecundaryTitle>
-              <ComponentParagraph>{item.Me }</ComponentParagraph>
-              <Component />
-             {item.companyName && <ComponentTitle> {item.companyName}</ComponentTitle> }
-
-              <ComponentParagraph>{item.development}</ComponentParagraph>
-              <ComponentParagraph>{item.design}</ComponentParagraph>
-              <ComponentTitle> {item.companyName}</ComponentTitle>
-              <ComponentSecundaryTitle> {item.time}</ComponentSecundaryTitle>
-              <ComponentParagraph> {item.aboutExperience}</ComponentParagraph>
-              <ComponentParagraph> {item.school}</ComponentParagraph>
-              <ComponentParagraph> {item.titleCourse}</ComponentParagraph>
-              <ComponentParagraph> {item.aboutCourse}</ComponentParagraph>
-              <ComponentLink href={item.link}>{item.scholl}</ComponentLink>
-            </Component>
-          </MainComponent>
-        );
-      })} */}
     </MainContent>
   );
 };
--- src/Components/MainDescribe/styles.js
+++ src/Components/MainDescribe/styles.js
@@ -1,9 +1,7 @@
 import styled from 'styled-components';
 
 
-export const MainTitle = styled.h2`
-font-size: 20px;
-`;
+
 
 export const MainContent = styled.main`
 display: flex;
@@ -11,20 +9,3 @@ flex-direction: column;
 width: 500px;
 
 `;
-
-export const MainComponent = styled.section`
-
-`;
-
-
-export const Component = styled.div`
-margin-bottom: 2vh;
-background-color: pink;
-`;
-
-export const ComponentTitle = styled.h3``;
-
-export const ComponentParagraph = styled.p``;
-
-export const ComponentSecundaryTitle = styled.h4``;
-export const ComponentLink = styled.a``;
