f3c3f99ff54c06bcd1d69e44fa30dbdd8cf7601f
linefmv <alinemaciel018@gmail.com>
2022-04-24 22:44:44 -0300

    style: change height

--- src/Components/Resume/styles.js
+++ src/Components/Resume/styles.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const Content = styled.aside`
   width: 250px;
-  height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
