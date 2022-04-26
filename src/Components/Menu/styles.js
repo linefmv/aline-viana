11a47aa0e8d034405e49705ecb46ab8a941581bf
linefmv <alinemaciel018@gmail.com>
2022-04-25 21:57:21 -0300

    style: set start style

--- src/Components/Menu/styles.js
+++ src/Components/Menu/styles.js
@@ -1,6 +1,25 @@
 import styled from "styled-components";
 
 export const Nav = styled.nav`
-height: 100px;
-background-color: pink;
+  height: 80px;
+padding-bottom: 10px;
+  background-color: pink;
+  display: flex;
+  align-items: flex-end;
+
+  ul {
+    width: 25%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
+
+  li {
+    list-style-type: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: #000;
+  }
 `;
