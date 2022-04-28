e2d41835d0de464f040756e2808b7c56d6a1aec2
linefmv <alinemaciel018@gmail.com>
2022-04-27 22:35:40 -0300

    fix: fix path to home

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -5,7 +5,7 @@ const MenuLeft = () => {
   return (
     <Nav>
       <ul>
-        <li><Link to="#">Home</Link></li>
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/projects">Projects</Link></li>
         <li><Link to="/hobbies">Hobbies</Link></li>
       </ul>
